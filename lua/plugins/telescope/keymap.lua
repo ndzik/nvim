92f29027dc23a32d2018abe5b045d0263cd5130a
Norbert Dzikowski <norbert@perun.network>
2021-03-22 13:15:24 +0100

    Reconfigure telescope grep command

--- lua/plugins/telescope/keymap.lua
+++ lua/plugins/telescope/keymap.lua
@@ -1,13 +1,13 @@
 local utils = require("utils")
 
 -- Keymapping for built-in stuff.
-utils.map('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
 utils.map('n', '<leader>fb', '<cmd>Telescope buffers<cr>')
 utils.map('n', '<leader>fh', '<cmd>Telescope help_tags<cr>')
 utils.map('n', '<leader>gc', '<cmd>Telescope git_commits<cr>')
 utils.map('n', '<leader>gs', '<cmd>Telescope git_status<cr>')
 
 -- Keymapping for custom telescope functions.
+utils.map('n', '<leader>fg', '<cmd>lua require("telescope.builtin").grep_string({ search = vim.fn.input("Grepping > ") })<cr>')
 utils.map('n', '<leader>gb', '<cmd>lua require("plugins.telescope.common").git_branches()<cr>')
 utils.map('n', '<leader>ev', '<cmd>lua require("plugins.telescope.common").search_dotfiles("$HOME/.config/nvim")<cr>')
 utils.map('n', '<leader>ff', '<cmd>lua require("plugins.telescope.common").find_files()<cr>')

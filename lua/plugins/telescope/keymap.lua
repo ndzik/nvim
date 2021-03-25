bbb8cba4355455e7c0baf0affb85ba2ded9cd24a
Norbert Dzikowski <norbert@perun.network>
2021-03-25 15:44:36 +0100

    Add note that ripgrep and fd are  dependencies

--- lua/plugins/telescope/keymap.lua
+++ lua/plugins/telescope/keymap.lua
@@ -8,8 +8,12 @@ utils.map('n', '<leader>gs', '<cmd>Telescope git_status<cr>')
 
 utils.map('n', '<leader>ho', '<cmd>Telescope hoogle<cr>')
 
--- Keymapping for custom telescope functions.
+-- TODO: Add a check or PR to Telescope to check, that `ripgrep` and `fd` are
+-- properly installed on the system, otherwise this command will always fail
+-- with a cryptic error message.
 utils.map('n', '<leader>fg', '<cmd>lua require("telescope.builtin").grep_string({ search = vim.fn.input("Grepping > ") })<cr>')
+
+-- Keymapping for custom telescope functions.
 utils.map('n', '<leader>gb', '<cmd>lua require("plugins.telescope.common").git_branches()<cr>')
 utils.map('n', '<leader>ev', '<cmd>lua require("plugins.telescope.common").search_dotfiles("$HOME/.config/nvim")<cr>')
 utils.map('n', '<leader>ff', '<cmd>lua require("plugins.telescope.common").find_files()<cr>')

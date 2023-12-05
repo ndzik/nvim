43e0338b41ded9345445687ff76ec99a4d96ce24
Norbert Dzikowski <norbert.dzikowski@yahoo.de>
2023-12-05 15:29:56 +0100

    lsp/python: Fix plugin configuration

--- lua/plugins/lsp/python.lua
+++ lua/plugins/lsp/python.lua
@@ -5,28 +5,31 @@ local custom_lsp_attach = function(client)
     common.DefaultKeymap()
     common.lsp_attach()
 
+    vim.lsp.set_log_level("debug")
     vim.api.nvim_command([[autocmd BufWritePre *.py lua vim.lsp.buf.format({timeout_ms = 2000})]])
 end
 
 require('lspconfig').pylsp.setup({
-    -- Make it compatible with virutalenv:
     settings = {
         pylsp = {
-            pyls_black = {
-                enabled = true,
-                exclude = { "**/venv/**" },
-            },
-            pyls_isort = {
-                enabled = true,
-                exclude = { "**/venv/**" },
-            },
-            pyls_mypy = {
-                enabled = true,
-                exclude = { "**/venv/**" },
-            },
-            pyls_flake8 = {
-                enabled = true,
-                exclude = { "**/venv/**" },
+            plugins = {
+                black = {
+                    enabled = true,
+                },
+                isort = {
+                    enabled = true,
+                },
+                pylsp_mypy = {
+                    enabled = true,
+                    ignore_missing_imports = true,
+                    live_mode = false,
+                },
+                flake8 = {
+                    enabled = false,
+                },
+                pycodestyle = {
+                    enabled = false,
+                },
             },
         },
     },

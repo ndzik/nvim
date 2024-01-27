b794fd1e83bdfa9a46af9691410f45500d5728ee
Norbert Dzikowski <norbert.dzikowski@yahoo.de>
2024-01-27 14:28:58 +0100

    lsp: use biome for formatting and TS/JS lsp

--- lua/plugins/lsp/biome.lua
+++ lua/plugins/lsp/biome.lua
@@ -2,10 +2,12 @@ local common = require("plugins.lsp.common")
 local capabilities = require("plugins.completion")
 
 local custom_lsp_attach = function(client)
-    common.DefaultKeymap()
     common.lsp_attach()
+
+    vim.api.nvim_command([[autocmd BufWritePre *.{ts,tsx,js,jsx} lua vim.lsp.buf.format({timeout_ms = 2000})]])
 end
 
-require("lspconfig").tsserver.setup({
+require("lspconfig").biome.setup({
     on_attach = custom_lsp_attach,
+    capabilities = capabilities,
 })

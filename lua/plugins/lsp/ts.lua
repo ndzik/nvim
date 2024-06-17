cadb90597c65ed737b26040eb0984a54891955b9
Norbert Dzikowski <norbert.dzikowski@yahoo.de>
2024-06-17 16:22:00 +0200

    lsp/ts: config for formatting and capabilities

--- lua/plugins/lsp/ts.lua
+++ lua/plugins/lsp/ts.lua
@@ -4,8 +4,11 @@ local capabilities = require("plugins.completion")
 local custom_lsp_attach = function(client)
     common.DefaultKeymap()
     common.lsp_attach()
+
+    vim.api.nvim_command([[autocmd BufWritePre *.{ts,js,jsx,tsx} lua vim.lsp.buf.format({timeout_ms = 2000})]])
 end
 
 require("lspconfig").tsserver.setup({
     on_attach = custom_lsp_attach,
+    capabilities = capabilities,
 })

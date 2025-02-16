57a083564291c7b70efb53a9af20b5f41bcbb444
Norbert Dzikowski <lambdax.one@icloud.com>
2025-02-16 08:11:49 +0100

    lsp/python: use pylsp again...

--- lua/plugins/lsp/python.lua
+++ lua/plugins/lsp/python.lua
@@ -9,6 +9,35 @@ local custom_lsp_attach = function(client)
     vim.api.nvim_command([[autocmd BufWritePre *.py lua vim.lsp.buf.format({timeout_ms = 2000})]])
 end
 
+require('lspconfig').pylsp.setup({
+    on_attach = custom_lsp_attach,
+    settings = {
+        pylsp = {
+            plugins = {
+                -- formatter
+                black = {
+                    enabled = false, -- we use ruff
+                },
+                -- import sorting
+                isort = {
+                    enabled = true,
+                },
+                pylsp_mypy = {
+                    enabled = true,
+                    ignore_missing_imports = true,
+                    live_mode = false,
+                },
+                flake8 = {
+                    enabled = false,
+                },
+                pycodestyle = {
+                    enabled = false,
+                },
+            },
+        }
+    }
+})
+
 require('lspconfig').ruff_lsp.setup({
     init_options = {
         settings = {
